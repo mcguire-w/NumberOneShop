b598763d4da749b1249e9462a80f5df3553c2b3e
chen-xp <1098583167@qq.com>
2019-03-27 16:06:38 +0800

    找回密码测试完成

--- app/js/api.js
+++ app/js/api.js
@@ -5,5 +5,6 @@ var obj = {
     login: host + '/%E9%A1%B9%E7%9B%AE/NumberOneShop/dist/login.html',
     register_name: host + 'one_server/register_name.php',
     login_query: host + 'one_server/login.php',
-    index:host +'/%E9%A1%B9%E7%9B%AE/NumberOneShop/dist/index.html'
+    index:host +'/%E9%A1%B9%E7%9B%AE/NumberOneShop/dist/index.html',
+    password_back:host+ 'one_server/password_back.php'
 }
--- app/js/password_back.js
+++ app/js/password_back.js
@@ -1,37 +1,48 @@
-var password_back = (function(){
-    
+var password_back = (function () {
 
 
-    return{
-        init(){
+
+    return {
+        init() {
 
 
 
             this.event()
         },
-        event(){
+        event() {
             const _this = this;
-            $('.ysame_input').one('click',function(){
-               _this.label()
+            $('.ysame_input').one('click', function () {
+                _this.label()
             })
             //获取焦点
-            $('.ysame_input').focus(function(){
+            $('.ysame_input').focus(function () {
                 $('#show').addClass('ifocus')
                 $('.y_tips_words').html('请输入手机号/邮箱/用户名')
             })
             //失去焦点
-            $('.ysame_input').blur(function(){
-                    $('#show').removeClass('ifocus')
-                    
+            $('.ysame_input').blur(function () {
+                $('#show').removeClass('ifocus')
+
             })
-            $('#confirmUser').click(function(){
-                alert('抱歉,此用户已被封停使用')
+            $('#confirmUser').click(function () {
+                console.log(1)
+                var val = $('.ysame_input').val()
+                $.get(obj.password_back, { user: val }, function (data) {
+                    data = JSON.parse(data);
+                    console.log(data)
+                    if (data.code == 0) {
+                        alert('错误，毛有东西')
+                    } else if (data.code == 200) {
+                        alert('密码为' + data.data.password)
+                    }
+
+                })
             })
-            
+
         },
         //span移动
-        label(){
-            $('.y_same_label').animate({'left':'-62px'},300)
+        label() {
+            $('.y_same_label').animate({ 'left': '-62px' }, 300)
         }
     }
 }())
